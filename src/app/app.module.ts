e77d7e32585209e99770a53cf81599a73dff71fd
Your Name <nhattybirhanu@gmail.com>
2022-01-20 09:49:01 -0700

    RegisterView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterViewComponent } from './register-view/register-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterViewComponent
   ],
   imports: [
     BrowserModule,
