8a5fac92ecf2184ed70aba8578e5f494e3772844
Your Name <nhattybirhanu@gmail.com>
2022-01-20 11:57:37 -0700

    RegView

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterViewComponent } from './register-view/register-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/register',component:RegisterViewComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterViewComponent } from './register-view/register-view.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatCardModule} from '@angular/material/card'
+import {MatIconModule} from '@angular/material/icon'
+import{MatInputModule} from '@angular/material/input'
+import{MatButtonModule} from '@angular/material/button'
+import{MatListModule} from '@angular/material/list'
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { RegisterViewComponent } from './register-view/register-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,MatButtonModule,MatListModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
